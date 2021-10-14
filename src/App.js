765971e15f57b297d675312e69e15816d9cd81b7
Rishabh Tiwari <rishabh.tiwari@reverieinc.com>
2021-10-14 22:35:39 +0530

    added search component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
+import { BiArchive } from 'react-icons/bi'
+import Search from './components/Search.js'
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="App conatiner mx-auto mt-3 font-thin">
+      <h1 className='text-5xl'><BiArchive className='inline-block text-red-400 align-top'/>Your Appointments</h1>
+      <Search />
     </div>
   );
 }
